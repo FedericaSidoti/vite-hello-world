c616cd80f77ccabd73063ae299e5f0d988ba5c3e
FedericaSidoti <federicasidoti21@gmail.com>
2023-11-03 15:15:39 +0100

    FIX- MESSAGE

---
+++ src/components/AppHero.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data() {
+    return {
+        message: 'La mia prima app con Vite!'
+    }
+    },
+    methods: {
+    }
+}
+
+</script>
+
+<template>
+    <div> {{ message }}</div>
+</template>
+
+<style scoped>
+
+</style>
